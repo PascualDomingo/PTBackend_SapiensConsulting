ee72eba52b1c371df94dc2224435b238c2a3b643
PascualDomingo <pascodom@gmail.com>
2024-04-20 22:53:10 -0600

    con jwt crear user y devolver token

--- backend_jwt/routes/api.php
+++ backend_jwt/routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ruta prueba hola mundo
+Route::get('/test', function(){
+    return 'hola mundo v2';
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('register', 'App\Http\Controllers\AuthController@register');
+});
